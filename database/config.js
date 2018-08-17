90bcb0f45348bdfb46f0a2e343100871fa9e7efa
Joyce Cho <joyjoycho@gmail.com>
2018-08-16 19:36:26 -0700

    connect to postgres database

--- database/config.js
+++ database/config.js
@@ -1,13 +1,35 @@
-
-const mysql = require('mysql');
-
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'review_db',
+const pg = require("pg");
+const db = new pg.Pool({
+  user: "user",
+  host: "127.0.0.1",
+  database: "postgres",
+  port: 5432
 });
 
-db.connect();
-
 module.exports = db;
+
+// db.query("select * from listings limit 10", (err, res) => {
+//   console.log(err, res);
+//   db.end();
+// });
+
+// pgp way
+// var pgp = require("pg-promise")();
+// var db = pgp("postgres://user@localhost:5432/postgres");
+//
+// db.any("select * from listings where id = 1").then(function(data) {
+//   console.log(data);
+// });
+
+// const mysql = require("mysql");
+//
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "review_db"
+// });
+//
+// db.connect();
+//
+// module.exports = db;
